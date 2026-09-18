5cd8073ab18db3f3b9bb6ce78d75532ea1cdc779
Lisa <171364917+Laloosa@users.noreply.github.com>
2026-09-18 12:46:58 +0100

    Create function to play a round and display result message. Change getHumanChoice() to make user input case-insensitive.

--- javascript.js
+++ javascript.js
@@ -12,15 +12,30 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    let choice = prompt("Enter rock, paper, or scissors: ");
-    if (choice == "Rock") {
+    let choice = prompt("Enter rock, paper, or scissors: ").toLowerCase();
+    let humanChoice = choice.replace(choice.charAt(0), (choice.charAt(0).toUpperCase()));
+    if (humanChoice == "Rock") {
         return "Rock";
     }
-    if (choice == "Paper") {
+    if (humanChoice == "Paper") {
         return "Paper";
     }
-    if (choice == "Scissors") {
+    if (humanChoice == "Scissors") {
         return "Scissors";
+    }
+}
+
+function playRound(humanChoice, computerChoice) {
+    if ((humanChoice == "Rock" && computerChoice == "Scissors") || (humanChoice == "Paper" && computerChoice == "Rock") || (humanChoice == "Scissors" && computerChoice == "Paper")) {
+        console.log("You win! " + humanChoice + " beats " + computerChoice + ".");
+        humanScore++;
+    }
+    else if ((humanChoice == "Rock" && computerChoice == "Paper") || (humanChoice == "Paper" && computerChoice == "Scissors") || (humanChoice == "Scissors" && computerChoice == "Rock")) {
+        console.log("You lose... " + humanChoice + " loses to " + computerChoice + ".");
+        computerScore++;
+    }
+    else {
+        console.log(humanChoice + " and " + computerChoice + ". It's a draw!");
     }
 }
 
